126b959c1296460099129c11fe887bdf476e4d0c
Sabith3938 <82069764+Sabith3938@users.noreply.github.com>
2023-07-16 12:42:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ Bot = Client(
     bot_token=Config.BOT_TOKEN,
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
-    workdir=Config.WORKDIR  # Add the required 'workdir' parameter
+    # Add the required 'workdir' parameter
 )
 
 
